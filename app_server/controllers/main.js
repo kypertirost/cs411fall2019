892f6177d12c96fecf1f5301e08724aa2bb9e589
kypertirost <liumiao1018@me.com>
2019-10-25 11:22:36 -0400

    run

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -4,19 +4,19 @@ request.open('GET', 'api.openweathermap.org/data/2.5/weather?q=London&appid=d6b7
 request.send()
 
 console.log(request.response)
-var data1 = JSON.parse(request.response)
-
-var weather = data1.weather;
-(weather.main).push
-// l = data1.length
-//   if(data1 && data1.length > 0) {
-//     list = ['<ul>'];
-//     for (i = 0; i < l; i ++) {
-//       list.push('<li ' + data1[i].weather.main + '</li>');
-//       list.push();
-//     }
-//   }
-//   list.push('</ul>')
+// var data1 = JSON.parse(request.response)
+//
+// var weather = data1.weather;
+// (weather.main).push
+// // l = data1.length
+// //   if(data1 && data1.length > 0) {
+// //     list = ['<ul>'];
+// //     for (i = 0; i < l; i ++) {
+// //       list.push('<li ' + data1[i].weather.main + '</li>');
+// //       list.push();
+// //     }
+// //   }
+// //   list.push('</ul>')
 
 
 module.exports.index = function (req, res) {
