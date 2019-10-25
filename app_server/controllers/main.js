7a060831abc8f8fc0c200e7dbbc8908661b38b46
kypertirost <liumiao1018@me.com>
2019-10-25 11:19:43 -0400

    buggy

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,22 +1,23 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest()
-request.open('GET', 'api.openweathermap.org/data/2.5/weather?q=London&appid=d6b790feb395d998105df9db1b50ece2', true)
+request.open('GET', 'api.openweathermap.org/data/2.5/weather?q=London&appid=d6b790feb395d998105df9db1b50ece2')
 request.send()
 
+console.log(request.response)
 var data1 = JSON.parse(request.response)
-var i, l, list;
-l = data1.length
-  if(data1 && data1.length > 0) {
-    list = ['<ul>'];
-    for (i = 0; i < l; i ++) {
-      list.push('<li ' + data1[i].weather.main + '</li>');
-      list.push();
-    }
-  }
-  list.push('</ul>')
 
-const app = document.getElementById('root')
-const data = document.createElement('text')
+var weather = data1.weather;
+(weather.main).push
+// l = data1.length
+//   if(data1 && data1.length > 0) {
+//     list = ['<ul>'];
+//     for (i = 0; i < l; i ++) {
+//       list.push('<li ' + data1[i].weather.main + '</li>');
+//       list.push();
+//     }
+//   }
+//   list.push('</ul>')
+
 
 module.exports.index = function (req, res) {
   res.render('index.html', { title: 'Express' });
