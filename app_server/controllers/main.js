db7e6c1838352b52695414e60a5b823131cd563d
kypertirost <liumiao1018@me.com>
2019-10-25 14:21:57 -0400

    need to add

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -1,7 +1,6 @@
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var request = new XMLHttpRequest()
-request.open('GET', 'api.openweathermap.org/data/2.5/weather?q=London&appid=d6b790feb395d998105df9db1b50ece2')
-request.send()
+
 
 console.log(request.response)
 // var data1 = JSON.parse(request.response)
