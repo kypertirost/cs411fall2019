31be268db648b9385d59cbccad47fd21b81cba06
Yuyang Huang <yuyang97@bu.edu>
2019-12-08 20:31:34 -0500

    added calendar under /public

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 var cookieSession = require('cookie-session');
 var passport = require('passport');
+var bodyParser = require("body-parser");
+
 
 
 const key = require('./config/key');
@@ -43,6 +45,9 @@ app.use('/auth', authRouter);
 app.use('/search', searchRouter);
 app.use('/privacy', privacyRouter);
 app.use('/profile', profileRouter);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(3000);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -64,6 +69,71 @@ app.get('/script', (req,res) =>{
   res.sendFile("./app_server/controllers/main.js")
 })
 
+app.get('/init', function(req, res){
+    db.event.insert({
+        text:"My test event A",
+        start_date: new Date(2019,12,8),
+        end_date:   new Date(2019,12,9)
+    });
+    db.event.insert({
+        text:"One more test event",
+        start_date: new Date(2019,12,10),
+        end_date:   new Date(2019,12,13),
+        color: "#DD8616"
+    });
 
+    /*... skipping similar code for other test events...*/
+
+    res.send("Test events were added to the database")
+});
+
+
+app.get('/data', function(req, res){
+    db.event.find().toArray(function(err, data){
+        //set id property for all records
+        for (var i = 0; i < data.length; i++)
+            data[i].id = data[i]._id;
+
+        //output response
+        res.send(data);
+    });
+});
+
+app.post('/data', function(req, res){
+    var data = req.body;
+
+    //get operation type
+    var mode = data["!nativeeditor_status"];
+    //get id of record
+    var sid = data.id;
+    var tid = sid;
+
+    //remove properties which we do not want to save in DB
+    delete data.id;
+    delete data["!nativeeditor_status"];
+
+
+    //output confirmation response
+    function update_response(err, result){
+        if (err)
+            mode = "error";
+        else if (mode == "inserted")
+            tid = data._id;
+
+        res.setHeader("Content-Type","application/json");
+        res.send({action: mode, sid: sid, tid: tid});
+
+    }
+
+    //run db operation
+    if (mode == "updated")
+        db.event.updateById( sid, data, update_response);
+    else if (mode == "inserted")
+        db.event.insert(data, update_response);
+    else if (mode == "deleted")
+        db.event.removeById( sid, update_response);
+    else
+        res.send("Not supported operation");
+});
 
 module.exports = app;
